0212ea1f2f27ef9101a9f869c3f19df64dc5f143
Dmytro_Romaniv <128440171+dimaromaniv@users.noreply.github.com>
2023-09-11 14:09:55 -0400

    first task passed

--- src/main/java/rocks/zipcode/io/quiz4/generics/MyStack.java
+++ src/main/java/rocks/zipcode/io/quiz4/generics/MyStack.java
@@ -1,25 +1,47 @@
 package rocks.zipcode.io.quiz4.generics;
 
+import java.util.Iterator;
+import java.util.Stack;
+
 /**
  * @author leon on 11/12/2018.
  */
-public class MyStack<SomeType> {
+public class MyStack<SomeType> implements Iterable  {
+
+    private Stack<SomeType> stack = new Stack<SomeType>();
+
+
+
     public MyStack() {
-        throw new UnsupportedOperationException("Method not yet implemented");
+//        this.stack = new MyStack<>();
+//        throw new UnsupportedOperationException("Method not yet implemented");
     }
 
     public Boolean isEmpty() {
-        return null;
+
+        return stack.isEmpty();
     }
 
     public void push(SomeType i) {
+
+        stack.push(i);
     }
 
     public SomeType peek() {
-        throw new UnsupportedOperationException("Method not yet implemented");
+          return  stack.peek();
+//        throw new UnsupportedOperationException("Method not yet implemented");
     }
 
     public SomeType pop() {
+//        if (!isEmpty()) {
+//            return stack.pop();
+//        }
+//        throw new UnsupportedOperationException("Stack is empty");
+        return stack.pop();
+    }
+
+    @Override
+    public Iterator iterator() {
         return null;
     }
 }
