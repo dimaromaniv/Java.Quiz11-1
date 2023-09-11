2e12ff01a1291de9e8fe1dc0095b15a23d109935
Dmytro_Romaniv <128440171+dimaromaniv@users.noreply.github.com>
2023-09-11 14:42:03 -0400

    pass something

--- src/main/java/rocks/zipcode/io/quiz4/generics/Group.java
+++ src/main/java/rocks/zipcode/io/quiz4/generics/Group.java
@@ -1,37 +1,65 @@
 package rocks.zipcode.io.quiz4.generics;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
+import java.util.function.Consumer;
 
 /**
  * @author leon on 18/12/2018.
  */
-public class Group<_> {
+public class Group<_> implements GroupInterface {
+
+    private List<_> groupList= new ArrayList();
     public Group() {
-        throw new UnsupportedOperationException("Method not yet implemented");
+
+
+//        throw new UnsupportedOperationException("Method not yet implemented");
     }
 
     public Integer count() {
-        return null;
+
+
+        return groupList.size();
     }
 
-    public void insert(_ value) {
-    }
-
-    public Boolean has(_ value) {
-        return null;
+    @Override
+    public Boolean has(Object valueToInsert) {
+        return groupList.contains(valueToInsert);
     }
 
     public _ fetch(int indexOfValue) {
-        return null;
+        return groupList.get(indexOfValue);
     }
 
-    public void delete(_ value) {
+    @Override
+    public void insert(Object string) {
+        groupList.add((_)string);
+    }
+
+
+    @Override
+    public void delete(Object valueToInsert) {
+            groupList.remove(valueToInsert);
     }
 
     public void clear() {
+        groupList.clear();
     }
 
     public Iterator<_> iterator() {
-        return null;
+
+        return groupList.stream().iterator();
+    }
+
+    @Override
+    public String toString() {
+        return ""+ groupList + "";
+
+    }
+
+    @Override
+    public void forEach(Consumer action) {
+        GroupInterface.super.forEach(action);
     }
 }
--- src/main/java/rocks/zipcode/io/quiz4/generics/SortedGroup.java
+++ src/main/java/rocks/zipcode/io/quiz4/generics/SortedGroup.java
@@ -1,18 +1,29 @@
 package rocks.zipcode.io.quiz4.generics;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author leon on 18/12/2018.
  */
 public class SortedGroup<_> extends Group<_> {
+
+//    private List<_> sortedList = (List<_>) new Group<>();
+    private List<_> sortedList;
+    public SortedGroup(){
+        this.sortedList = new ArrayList<>();
+    }
     @Override
-    public void insert(_ value) {
+    public void insert(Object value) {
+        sortedList.add((_)value);
     }
 
     @Override
-    public void delete(_ value) {
+    public void delete(Object value) {
+        sortedList.remove((_)value);
     }
 
     public Integer indexOf(_ value) {
-        return null;
+        return sortedList.indexOf(value);
     }
 }
